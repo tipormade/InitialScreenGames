3ce8f3194f87d08347b54a59308195ae0d2316d8
Toddynn <viniciusgabriez07@gmail.com>
2022-09-09 12:37:49 -0300

    Update minimax.js

--- Jogo-Da-Velha-Main/ia-minimax/minimax.js
+++ Jogo-Da-Velha-Main/ia-minimax/minimax.js
@@ -1,11 +1,4 @@
-// Tic Tac Toe AI with Minimax Algorithm
-// The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/CodingChallenges/154-tic-tac-toe-minimax.html
-// https://youtu.be/I64-UTORVfU
-// https://editor.p5js.org/codingtrain/sketches/0zyUhZdJD
-
 function bestMove() {
-  // AI to make its turn
   let bestScore = -Infinity;
   let move;
   for (let i = 0; i < 3; i++) {
@@ -15,7 +8,7 @@ function bestMove() {
         board[i][j] = ai;
         let score = minimax(board, 0, false);
         board[i][j] = '';
-        let random = Math.ceil(Math.random() * 6);
+        let random = Math.ceil(Math.random() * 5);
         if (random > 1) {
           if (score > bestScore) {
             bestScore = score;
@@ -49,7 +42,6 @@ function minimax(board, depth, isMaximizing) {
     let bestScore = -Infinity;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        // Is the spot available?
         if (board[i][j] == '') {
           board[i][j] = ai;
           let score = minimax(board, depth + 1, false);
@@ -63,7 +55,6 @@ function minimax(board, depth, isMaximizing) {
     let bestScore = Infinity;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        // Is the spot available?
         if (board[i][j] == '') {
           board[i][j] = human;
           let score = minimax(board, depth + 1, true);
