d13732a99c0f19a7e48d316d844139eb106360eb
EmanoelViniciusCastanha <111919840+EmanoelViniciusCastanha@users.noreply.github.com>
2022-12-06 07:52:02 -0300

    Local salvo errado
    Local da pasta responsivo tablet foi salvo errado.

--- ResponsivoTablet/InitialScreenGames-main/js/removecookies.js
+++
@@ -1,9 +0,0 @@
-
-function removeItem(sKey, sPath, sDomain) {
-    document.cookie = encodeURIComponent(sKey) + 
-                  "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + 
-                  (sDomain ? "; domain=" + sDomain : "") + 
-                  (sPath ? "; path=" + sPath : "");
-}
-
-removeItem("_form_fields");
